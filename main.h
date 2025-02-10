f179a08e9c38bc2d03d087075f86ed8ee7160a91
Nomlc <156930639+Nomlc@users.noreply.github.com>
2025-02-10 13:06:23 +0100

    Add files via upload

--- main.h
+++ main.h
@@ -22,9 +22,10 @@ void init_screen(WINDOW **g_win, WINDOW **ui_win);
 void child_task(int i, WINDOW **g_win, int pipefd[], int pipefd_projectiles[], int pipefd_grenade[], rvr r);
 void end_screen(WINDOW **g_win, WINDOW **ui_win);
 bool isDrawning(pos f, msg *c, int nspeeds);
+bool isShot (int proj_active, pos f, msg proj);
 bool den(bool dens[5], pos frog_pos);
 void printDens(WINDOW **win, bool dens[NDENS]);
-gstate collisions(msg msgs[], bool dens[NDENS]);
+gstate collisions(msg msgs[], bool dens[NDENS], int proj_active);
 gstate game(WINDOW **g_win, bool dens[NDENS]);
 
 #endif
